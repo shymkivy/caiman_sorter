7aa964a3b7745e3cf9b7f6c1ce50f4f8e3142c32
yuriy <shymkivy@gmail.com>
2022-04-08 18:04:34 -0400

    updates

--- caiman_sorter_functions/deconvolution_dep/f_cs_compute_MCMC.m
+++ caiman_sorter_functions/deconvolution_dep/f_cs_compute_MCMC.m
@@ -24,7 +24,7 @@ elseif params.p == 2
     end
 end
 
-SAMP = f_cs_compute_MCMC_core(y, params);
+[SAMP, spikeRaster] = f_cs_compute_MCMC_core(y, params);
 
 if app.SaveSamplesOutputsMCMC.Value
     app.proc.deconv.MCMC.SAMP{n_cell} = SAMP;
