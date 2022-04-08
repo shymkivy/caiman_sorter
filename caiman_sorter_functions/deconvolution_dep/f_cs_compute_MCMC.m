df8beec24b78ce73aaf9412bdf343bf21d46839c
yuriy <shymkivy@gmail.com>
2022-04-08 15:20:44 -0400

    updates

--- caiman_sorter_functions/deconvolution_dep/f_cs_compute_MCMC.m
+++ caiman_sorter_functions/deconvolution_dep/f_cs_compute_MCMC.m
@@ -32,7 +32,7 @@ end
 
 if SAMP.process_ok
     % need to fill in the zeros here for unprocessed signal
-    app.proc.deconv.MCMC.C{n_cell} = [zeros(1,sig_start-1), mean(SAMP.C_rec,1)];
+    app.proc.deconv.MCMC.C{n_cell} = [zeros(1,SAMP.sig_start-1), mean(SAMP.C_rec,1)];
     app.proc.deconv.MCMC.S{n_cell} = mean(spikeRaster,1);
 end
 %     figure; imagesc(spikeRaster)
